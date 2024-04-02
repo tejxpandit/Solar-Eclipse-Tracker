8ed087751ee66cbf9a03b93007dce489ffeafe06
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-04-01 23:22:49 -0500

    Reading Video Streams using OpenCV
    Using the openCV library to read video streams from local streaming devices given their IP and port.

---
+++ test/test_Mobile_webcam_server.py
@@ -0,0 +1,19 @@
+# File : Test : Mobile Phone Webcam Server
+# Author : Tej Pandit
+# Date : April 2024
+# NOTE : Tested with Android App [ScreenStream by Dmytro Kryvoruchko]
+
+import torch
+import cv2
+from time import sleep
+
+URL = "http://" + "192.168.0.105" + ":8080/stream.mjpeg"
+
+# Model
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5m')  # Default Device GPU
+#model = torch.hub.load('ultralytics/yolov5', 'yolov5m', device="cpu") # Run with CPU
+
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(URL)
+#webcam = cv2.VideoCapture(0)
+sleep(2)
