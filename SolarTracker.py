770189addb7e26826671e4d6759f63a64953c6a8
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-05-16 20:50:55 -0500

    Setting Date and Timezone
    Added a function to add the date and timezone.

--- SolarTracker.py
+++ SolarTracker.py
@@ -13,9 +13,16 @@ class SolarTracker:
         self.date_time = None
         self.day_routine = None
 
+    def setDateTime(self):
+        self.date_time = datetime.now(tz=ZoneInfo("America/Chicago"))
+        # self.date_time = datetime(2024, 4, 4, 18, 0, 0, 0, tzinfo=ZoneInfo("America/Chicago"))
+
+
     # TEST : GENERATE SOLAR ECLIPSE ROUTINE
     def generateDayRoutine(self):
         self.day_routine = []
         for h in range(8, 18, 1): # hours
             for m in range(0, 60, 10): # minutes
                 self.day_routine.append(datetime(2024, 4, 4, h, m, 0, 0, tzinfo=ZoneInfo("America/Chicago")))
+
+    
