99abe8dba2c17ef6c7ac633793b6d4654a5fca21
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-06-24 17:23:14 +0530

    Create Bluetooth Controller Window
    A function to create the BT controller window. It adds buttons for connecting and disconnecting from the BT module and also binds special indicative themes to the buttons.

--- MobileBTController.py
+++ MobileBTController.py
@@ -16,3 +16,11 @@ class MobileBTController:
         self.bt_state = False
         self.command = ""
         self.theme = SolarTrackerThemes()
+
+    # BT Controller Window
+    def createBTControllerWindow(self):
+        dpg.add_window(label="Mobile Camera Bluetooth Controller", tag="mobile_controller_window")
+        dpg.add_button(label="Connect BT", parent="mobile_controller_window", tag="start_bt_button", callback=self.startBTCom)
+        dpg.add_button(label="Disconnect BT", parent="mobile_controller_window", tag="stop_bt_button", show=False, callback=self.stopBTCom) 
+        dpg.bind_item_theme("start_bt_button", self.theme.enableGreen())
+        dpg.bind_item_theme("stop_bt_button", self.theme.disableRed())
