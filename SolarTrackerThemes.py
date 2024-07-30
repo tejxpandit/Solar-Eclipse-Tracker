56a53cfdb89ae6b7baeaa9f8f045f8e70235fab3
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-07-30 22:41:17 +0530

    Solar Tracker Themes
    Created a class for custom themes for the solar tracker app.

---
+++ SolarTrackerThemes.py
@@ -0,0 +1,10 @@
+# File : Solar Tracker App Themes
+# Author : Tej Pandit
+# Date : July 2024
+
+import dearpygui.dearpygui as dpg
+
+class SolarTrackerThemes:
+    def __init__(self):
+        self.varName = None
+
