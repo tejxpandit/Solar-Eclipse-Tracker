7ab1ed7f0caed91f06f1839af9bb824dc988674b
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-07-31 14:19:35 +0530

    Blue Theme for Navigation Buttons
    Created a blue theme for the navigation buttons.

--- SolarTrackerThemes.py
+++ SolarTrackerThemes.py
@@ -8,3 +8,11 @@ class SolarTrackerThemes:
     def __init__(self):
         self.varName = None
 
+    def navBlue(self):
+        with dpg.theme() as theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Button, 			(48, 100, 153, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered,   (84, 159, 235, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive,    (84, 159, 235, 200))
+        return theme
+    
